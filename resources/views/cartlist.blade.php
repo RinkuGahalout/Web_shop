84ec30ffb1aeb23edc383c10edf9ab4bd80e1d2e
Rinku Gahalout <thakurrinku02526@gmail.com>
2024-11-04 17:52:40 +0530

    remove product from cart

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -6,20 +6,20 @@
             <h2>Cartlist Products</h2>
             <div class="">
                 @foreach($products as $index => $item)
-                    <div class="row searched-item">
-                        <div class="col-sm-3">
+                    <div class="row searched-item cart-list-driver">
+                        <div class="col-sm-4 cart-list-image">
                             <a href="detail/{{$item->id}}">
                                 <img class="tranding-img" src="{{ $item->gallery }}">
                             </a>
                         </div>
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <div class="" >
                                 <h2>{{ $item->name}}</h2>
                                 <h4>{{ $item->description}}</h4>
                             </div>
                         </div>
-                        <div class="col-sm-3">
-                                <button class="btn btn-danger">Remove from cart</button>
+                        <div class="col-sm-4">
+                            <a href="/removecart/{{$item->cart_id}}" class="btn btn-danger">Remove from car </a>
                         </div>
                     </div>     
                 @endforeach
